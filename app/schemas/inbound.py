2be5c5ecacd1da3129f3c96fe7f73442728991c2
5amp12 <samuel.passey@hotmail.com>
2026-07-17 18:48:49 +0100

    feat: checks the board meeting events api call is shaped correctly

--- app/schemas/inbound.py
+++ app/schemas/inbound.py
@@ -1,24 +1,79 @@
-#Checks the JSON is shaped correctly --Currently Rough cut--
+#Checks the JSON is shaped correctly 
 
-from enum import Enum
 from pydantic import BaseModel
+from typing import Optional, Any, Literal
+from datetime import datetime, date
 
-class Speaker(str, Enum):
-    clinician = "clinician"
-    patient = "patient"
 
-class TranscriptSegment(BaseModel):
-    speaker: Speaker
+class AdminSegment(BaseModel):
+    id: int
+    name: str
+    title: str
+
+class DiagnosisTypeSegment(BaseModel):
+    id: int
+    code: str
+    display_name: str
+
+class MedicalFileSegment(BaseModel):
+    id: int
+    name: str
+    url: str
+    mime: str
+
+class RecommendationSegment(BaseModel):
+    id: int
     text: str
-    start: float
-    end: float
-    confidence: float
+    author: str
+
+class DiagnosticInstanceSegment(BaseModel):
+    id: int
+    field_values: dict[str, Any]  #May be multiple fields
+
+class PatientGeneralSegement(BaseModel):
+    id: int
+    first_name: str
+    last_name: str
+
+
+class BoardSection(BaseModel):
+    id: int
+    title: str
+    date: datetime
+    status: Literal["Completed"]
+    admin: AdminSegment
+
+class ParticipantSection(BaseModel):
+    id: int
+    name: str
+    email: str
+    title: str
+
+class PatientSection(BaseModel):
+    id: int
+    mrn: str
+    name: str
+    gender: str
+    date_of_birth: date
+    diagnosis_type: DiagnosisTypeSegment
+    medical_files: list[MedicalFileSegment]
+    recommendations: Optional[list[RecommendationSegment]]   #Might not be optional
+    diagnosticInstances: list[DiagnosticInstanceSegment]
+
+class EventSection(BaseModel):
+    id: int
+    event_type: str
+    event_timestamp: datetime
+    actor_name: str
+    patient: Optional[PatientGeneralSegement] = None
+    payload: Optional[dict[str, Any]] = None 
+
 
 class InboundVisitPayload(BaseModel):
-    visit_id: str
-    patient_id: str
-    clinician_id: str
-    transcript: list[TranscriptSegment]
-    consent_to_record: bool
-    consent_to_process: bool
-    idempotency_key: str
+    success: Literal[True]
+    board: BoardSection
+    participants: list[ParticipantSection]
+    patients: list[PatientSection]
+    events: list[EventSection]
+
+    
